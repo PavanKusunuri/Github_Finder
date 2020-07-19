d6451b0b1e81faacd721e6be032211a8049f71ba
Pavan <pavantejakusunuri@gmail.com>
2020-07-19 10:17:17 +0530

    Added Search Component in Application

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+
 import axios from "axios";
 
 class App extends Component {
@@ -13,7 +15,7 @@ class App extends Component {
   async componentDidMount() {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
     );
     this.setState({ users: res.data, loading: false });
   }
@@ -21,6 +23,7 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fa fa-github" />
+        <Search />
         <div className="container">
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
